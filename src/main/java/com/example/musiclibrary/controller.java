ed33ac3630fe95edff87a9aa646e18ed7d6b1d85
Ajay Varma <128949987+ajayvarma007789@users.noreply.github.com>
2024-06-03 19:30:41 +0530

    Update controller.java

--- src/main/java/com/example/musiclibrary/controller.java
+++ src/main/java/com/example/musiclibrary/controller.java
@@ -4,19 +4,11 @@ import java.sql.*;
 
 //jdbc connectivity: this is used to connect to the database 
 public class controller {
-    public static void main(String[] args) {
-        String sql = "select title from songs where song_id=2";
-        String url = "jdbc:mysql://127.0.0.1:3306/Music_Library";
-        String username = "root";
-        String password = "password";
+    private final Connection conn;
 
-        try {
-            Connection con = DriverManager.getConnection(url, username, password);
-            Statement st = con.createStatement();
-            st.executeQuery(sql);
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
+    public Database() throws SQLException, ClassNotFoundException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        this.conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/lifecanvas", "lifecanvas", "lifecanvas");
     }
 }
 
